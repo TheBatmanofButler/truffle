fab6ed6590daee6da8798b1ec8ce44a8c034b4cc
Amol Kapoor <theahura@gmail.com>
2017-06-13 16:08:40 -0400

    Added install package framework

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='truffle',
+      version='0.1',
+      description='Code search, documentation aid, code visualizer',
+      url='',
+      author='1traintech',
+      author_email='onetraintech@gmail.com',
+      packages=['truffle'],
+      dependency_links=[],
+      install_requires=[],
+      zip_safe=False)
