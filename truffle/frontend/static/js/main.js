9f4a49d49dc21976e3534086feaad4e8e1a553a1
Ganesh Ravichandran <gr2483@columbia.edu>
2017-06-21 22:44:56 -0400

    added html and javascript codemirror stuff

--- truffle/frontend/static/js/main.js
+++ truffle/frontend/static/js/main.js
@@ -8,17 +8,20 @@ $(".file-panel li").click(function (e) {
 });
 
 $( document ).ready(function() {
-
+	console.log(21)
 	var mode;
 
 	if (filename.includes(".py")) {
 		mode = "python";
+		console.log("py")
 	}
 	else if (filename.includes(".js")) {
 		mode = "javascript";
+		console.log("js")
 	}
 	else if (filename.includes(".html")) {
-		mode = "html";
+		mode = "xml";
+		console.log("xml")
 	}
 
 	var editor = CodeMirror(document.getElementById("editor"), {
